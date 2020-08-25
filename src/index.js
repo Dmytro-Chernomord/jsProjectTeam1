21766d245b2151ffedc894c35ab2fdc85c382df6
Dmytro Chernomord <tschernomord@gmail.com>
2020-08-25 17:55:37 +0200

    main fetch

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './styles.css';
 import './sass/basic.scss';
+import createMain from './js/createMain.js';
---
+++ src/js/createMain.js
@@ -0,0 +1,20 @@
+import movies from '../template/movies.hbs';
+
+const apiKey = '89b9004c084fb7d0e8ffaadd17cb8254';
+const inputRef = document.querySelector('#input');
+function createMain(search) {
+  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1&query=${search}`;
+  return fetch(url).then(res => res.json());
+}
+
+function insertHtml() {
+  createMain().then(data => {
+    const year = data.results.release_date;
+    console.log(data.results[0].id);
+    document.querySelector('main').innerHTML = movies(data.results);
+    console.log(year);
+  });
+}
+
+insertHtml();
+console.log(movies);
