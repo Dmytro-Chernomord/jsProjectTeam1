66777151073f87378ed29a860b468903e9ecfbc6
Nataliia <n3v3st4@gmail.com>
2020-08-27 00:47:45 +0300

    clicking on btns in movie-card rewrites the localstorage and changes styles and text of the buttons

--- src/js/one_movie_main.js
+++ src/js/one_movie_main.js
@@ -7,11 +7,13 @@ import { toggleModal } from './togglePage.js';
 // ----Функция для отрисовки модалки
 function generateOneMovieMarkup(id) {
   spinnerOn();
-  apiService.getOneMovieInfo(id).then(data => {
-    refs.movieCard.innerHTML = movie([data]);
-    onListenerBtn(data.id);
-    console.log(data.id);
-  })
+  apiService
+    .getOneMovieInfo(id)
+    .then(data => {
+      refs.movieCard.innerHTML = movie([data]);
+      onListenerBtn(data.id);
+      //   console.log(data.id);
+    })
     .catch(() => errorOn())
     .finally(() => spinnerOff());
 }
@@ -27,6 +29,7 @@ function onMovieCardClick(event) {
   if (clickedItem.nodeName === 'UL') return;
   refs.modal.classList.toggle('is-hidden');
   generateOneMovieMarkup(clickedItem.dataset.id);
+  setTimeout(checkLocalStorage, 500, clickedItem.dataset.id);
 }
 // -----------------------------------------------------------------
 function onListenerBtn(id) {
@@ -35,25 +38,27 @@ function onListenerBtn(id) {
       return;
     }
     let addWatched = event.target.dataset.source;
-    console.log(event.target);
+    // console.log(event.target);
     if (addWatched === 'add-watched') {
       addLocalStorage('add-watched', id);
     } else if (addWatched === 'add-queue') {
       addLocalStorage('add-queue', id);
     }
+    checkLocalStorage(id);
   });
 }
 
 function addLocalStorage(key, id) {
   let list = [];
   let parseLocalStorage = JSON.parse(localStorage.getItem(key));
-
   if (parseLocalStorage === null) {
     list.push(id);
     localStorage.setItem(key, JSON.stringify(list));
   } else {
     if (parseLocalStorage.includes(id)) {
-      return;
+      list = parseLocalStorage;
+      list.splice(list.indexOf(id), 1);
+      localStorage.setItem(key, JSON.stringify(list));
     } else {
       list = parseLocalStorage;
       list.push(id);
@@ -61,6 +66,39 @@ function addLocalStorage(key, id) {
     }
   }
 }
+
+// ----- Вешаем слушатель на крестик в модалке --------
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+// ------
+//
+// ------ Функция для проверки наличия id фильма в local storage
+
+function checkLocalStorage(id) {
+  let arrWatched = JSON.parse(localStorage.getItem('add-watched'));
+  let arrQueue = JSON.parse(localStorage.getItem('add-queue'));
+  let watchBtn = document.querySelector('.watch-btn');
+  let queueBtn = document.querySelector('.queue-btn');
+  //   console.log(id);
+  //   console.log(arrWatched);
+  //   console.log(arrQueue);
+  if (arrWatched.includes(Number(id))) {
+    watchBtn.textContent = 'remove  "watched"';
+  } else {
+    watchBtn.textContent = 'add to watched';
+  }
+  if (arrQueue.includes(Number(id))) {
+    queueBtn.textContent = 'remove  "queue"';
+  } else {
+    queueBtn.textContent = 'add to queue';
+  }
+}
+
+//
+// ------ Функция для изменения текста кнопки в зависимости от наличия id
+//
+// ------ Функция для
+
 // let all = [];
 // function generateMovie() {
 //   let obj = JSON.parse(localStorage.getItem('add-watched'));
@@ -74,6 +112,3 @@ function addLocalStorage(key, id) {
 //   });
 // }
 // generateMovie();
-
-// ----- Вешаем слушатель на крестик в модалке --------
-refs.closeModalBtn.addEventListener('click', toggleModal);
