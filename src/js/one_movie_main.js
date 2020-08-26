ca8c4c1553474008a1c5816c36cdf071a115af11
Shevchenko Elena <elena.kadru@gmail.com>
2020-08-26 17:57:15 +0300

    added modal on click to card

--- src/js/one_movie_main.js
+++ src/js/one_movie_main.js
@@ -1,22 +1,26 @@
 import movie from '../template/one_movie.hbs';
 import apiService from './apiServices.js';
 import refs from './refs.js';
+import { toggleModal } from './togglePage.js';
 
-// ----Функция для перерисовки страницы по клику
-
+// ----Функция для отрисовки модалки
 function generateOneMovieMarkup(id) {
   apiService.getOneMovieInfo(id).then(data => {
-    refs.gallery.innerHTML = movie([data]);
+    refs.movieCard.innerHTML = movie([data]);
   });
 }
 
 // ----- Вешаем слушатель на список --------
 refs.gallery.addEventListener('click', onMovieCardClick);
 
+// ----Функция для открытия модалки
 function onMovieCardClick(event) {
   event.preventDefault();
   let clickedItem = event.target;
 
   if (clickedItem.nodeName === 'UL') return;
+  refs.modal.classList.toggle('is-hidden');
   generateOneMovieMarkup(clickedItem.dataset.id);
 }
+// ----- Вешаем слушатель на крестик в модалке --------
+refs.closeModalBtn.addEventListener('click', toggleModal);
