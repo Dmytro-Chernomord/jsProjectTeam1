94320975723df3e09172edfc5acf6a39f6db640e
Karina-Sapezhko <66127579+Karina-Sapezhko@users.noreply.github.com>
2020-08-28 01:19:40 +0300

    btn close---update markup

--- src/js/library-markup.js
+++ src/js/library-markup.js
@@ -3,15 +3,23 @@ import refs from './refs.js';
 import apiService from './apiServices.js';
 import { checkTotalItems } from './pagination.js';
 
-refs.myLib.addEventListener('click', () => updateMarkup('add-watched'));
+// -----------------слушалель на myLibrary, btn watched, btn queue
+refs.myLib.addEventListener('click', () => {
+  updateAccentBtn();
+  updateMarkup('add-watched');
+  // замена кнопки close в modal при переходе в library
+  refs.libraryBtnClose.classList.remove('is-hidden-btn');
+  refs.closeModalBtn.classList.add('is-hidden-btn');
+});
 refs.watchedBtn.addEventListener('click', () => updateMarkup('add-watched'));
 refs.queueBtn.addEventListener('click', () => updateMarkup('add-queue'));
 
+// -------------------------------обновляет разметку
 function updateMarkup(str) {
   refs.gallery.innerHTML = '';
   generateMovieLibrary(str);
 }
-
+// --------------------------парсит localStorage и генерит список карточек
 function generateMovieLibrary(str) {
   let obj = JSON.parse(localStorage.getItem(str));
   checkTotalItems(obj);
@@ -24,12 +32,26 @@ function generateMovieLibrary(str) {
   }
 }
 
-// ----------------Переключатель цвета между кнопоками library -----
+// ----------------Переключатель цвета между кнопоками library
 refs.queueBtn.addEventListener('click', () => {
   refs.watchedBtn.classList.remove('btn--accent');
   refs.queueBtn.classList.add('btn--accent');
 });
 refs.watchedBtn.addEventListener('click', () => {
+  updateAccentBtn();
+});
+function updateAccentBtn() {
   refs.watchedBtn.classList.add('btn--accent');
   refs.queueBtn.classList.remove('btn--accent');
-});
+}
+// -----------замена кнопки close в modal при переходе из library
+function replaseBtnModal() {
+  refs.libraryBtnClose.classList.add('is-hidden-btn');
+  refs.closeModalBtn.classList.remove('is-hidden-btn');
+}
+
+export default {
+  updateAccentBtn,
+  updateMarkup,
+  replaseBtnModal,
+};
--- src/js/one_movie_main.js
+++ src/js/one_movie_main.js
@@ -6,6 +6,7 @@ import apiService from './apiServices.js';
 import refs from './refs.js';
 import { errorOn, spinnerOff, spinnerOn } from './spinner.js';
 import { toggleModal } from './togglePage.js';
+import libraryClick from './library-markup.js';
 
 // ----Функция для отрисовки модалки
 function generateOneMovieMarkup(id) {
@@ -143,6 +144,7 @@ function addLocalStorage(key, id) {
 
 // ----- Закрытие модалки - Вешаем слушатель на крестик в модалке, тоглим класс is-hidden --------
 refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.libraryBtnClose.addEventListener('click', toggleModal);
 
 // ------  инициализируем в localStorage массив для хранения id фильмов, если localStorage таких ключей не содержит
 
@@ -182,6 +184,10 @@ function checkLocalStorage(id) {
     queueBtn.textContent = 'add to queue';
   }
 }
+// function onListenerBtn(id) {
+//   fn1(id)
+//   fn2(id)
+// }
 
 function onListenerBtn(id) {
   let arrWatched = JSON.parse(localStorage.getItem('add-watched'));
@@ -206,6 +212,7 @@ function onListenerBtn(id) {
       watchedArr.splice(watchedArr.indexOf(id), 1);
       localStorage.setItem('add-watched', JSON.stringify(watchedArr));
       checkLocalStorage(id);
+      addNewObjInLibrary('add-watched');
     }
   });
 
@@ -225,6 +232,13 @@ function onListenerBtn(id) {
       queueArr.splice(queueArr.indexOf(id), 1);
       localStorage.setItem('add-queue', JSON.stringify(queueArr));
       checkLocalStorage(id);
+      addNewObjInLibrary('add-queue');
     }
+  });
+}
+// ---------При закрытии модалки создается новый список в library--------
+function addNewObjInLibrary(str) {
+  refs.libraryBtnClose.addEventListener('click', () => {
+    libraryClick.updateMarkup(str);
   });
 }
