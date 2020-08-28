0d21321373e9863f0a0f03492359842c036c2ee1
Dmytro <tschernomord@gmail.com>
2020-08-28 20:43:21 +0200

    darkTheme v1

---
+++ src/js/darkTheme.js
@@ -0,0 +1,52 @@
+const refs = {
+  buttonTheme: document.querySelector('#btn-theme'),
+  main: document.querySelector('main'),
+  divtheme: document.querySelector('.movie-card'),
+};
+console.log(refs.divtheme);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+refs.buttonTheme.addEventListener('click', themeChange);
+
+getStorage();
+function getStorage() {
+  const themeCheck = localStorage.getItem('Theme:');
+  if (themeCheck === null || themeCheck === 'light-theme') {
+    setLocalStorage(Theme.LIGHT);
+    lightTheme();
+  } else if (themeCheck === 'dark-theme') {
+    darkTheme();
+  }
+}
+
+function darkTheme() {
+  refs.main.classList.toggle('darkTheme');
+  refs.divtheme.classList.toggle('darkTheme');
+
+  //   refs.main.classList.remove('light-theme');
+  refs.buttonTheme.checked = true;
+}
+
+// function lightTheme() {
+//   refs.main.classList.remove('darkTheme');
+//   //   refs.main.classList.add('light-theme');
+// }
+
+function themeChange() {
+  if (refs.buttonTheme.checked) {
+    darkTheme();
+    setLocalStorage(Theme.DARK);
+  } else {
+    darkTheme();
+
+    // lightTheme();
+    setLocalStorage(Theme.LIGHT);
+  }
+}
+
+function setLocalStorage(info) {
+  localStorage.setItem('Theme:', info);
+}
