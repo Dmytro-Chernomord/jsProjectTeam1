313b30c0f49c7b91c22f5ba92f25613cf99fcd39
Dmytro <tschernomord@gmail.com>
2020-08-29 10:52:18 +0200

    darkTheme need fix footer in modal

--- src/js/darkTheme.js
+++ src/js/darkTheme.js
@@ -16,18 +16,7 @@ const Theme = {
 };
 refs.buttonTheme.addEventListener('click', themeChange);
 
-getStorage();
-function getStorage() {
-  const themeCheck = localStorage.getItem('Theme:');
-  if (themeCheck === null || themeCheck === 'light-theme') {
-    setLocalStorage(Theme.LIGHT);
-    lightTheme();
-  } else if (themeCheck === 'dark-theme') {
-    darkTheme();
-  }
-}
-
-function darkTheme() {
+function themeChange() {
   refs.main.classList.toggle('darkTheme');
   refs.divtheme.classList.toggle('darkTheme');
   refs.labbel.classList.toggle('darkTheme');
@@ -38,33 +27,58 @@ function darkTheme() {
   refs.modalBG.classList.toggle('darkTheme');
 
   //   refs.main.classList.remove('light-theme');
-  refs.buttonTheme.checked = true;
+  // refs.buttonTheme.checked = true;
 }
 
-function lightTheme() {
-  refs.main.classList.toggle('darkTheme');
-  refs.divtheme.classList.toggle('darkTheme');
-  refs.labbel.classList.toggle('darkTheme');
-  refs.labbel1.classList.toggle('darkTheme');
-  refs.labbel2.classList.toggle('darkTheme');
-  refs.modalText.classList.toggle('darkTheme');
-  refs.modalCross.classList.toggle('darkTheme');
-  refs.modalBG.classList.toggle('darkTheme');
-  refs.buttonTheme.checked = false;
-}
+// getStorage();
+// function getStorage() {
+//   const themeCheck = localStorage.getItem('Theme:');
+//   if (themeCheck === null || themeCheck === 'light-theme') {
+//     setLocalStorage(Theme.LIGHT);
+//     lightTheme();
+//   } else if (themeCheck === 'dark-theme') {
+//     darkTheme();
+//   }
+// }
 
-function themeChange() {
-  if (refs.buttonTheme.checked) {
-    darkTheme();
-    setLocalStorage(Theme.DARK);
-  } else {
-    darkTheme();
+// function darkTheme() {
+//   refs.main.classList.toggle('darkTheme');
+//   refs.divtheme.classList.toggle('darkTheme');
+//   refs.labbel.classList.toggle('darkTheme');
+//   refs.labbel1.classList.toggle('darkTheme');
+//   refs.labbel2.classList.toggle('darkTheme');
+//   refs.modalText.classList.toggle('darkTheme');
+//   refs.modalCross.classList.toggle('darkTheme');
+//   refs.modalBG.classList.toggle('darkTheme');
 
-    // lightTheme();
-    setLocalStorage(Theme.LIGHT);
-  }
-}
+//   //   refs.main.classList.remove('light-theme');
+//   // refs.buttonTheme.checked = true;
+// }
 
-function setLocalStorage(info) {
-  localStorage.setItem('Theme:', info);
-}
+// function lightTheme() {
+//   refs.main.classList.toggle('darkTheme');
+//   refs.divtheme.classList.toggle('darkTheme');
+//   refs.labbel.classList.toggle('darkTheme');
+//   refs.labbel1.classList.toggle('darkTheme');
+//   refs.labbel2.classList.toggle('darkTheme');
+//   refs.modalText.classList.toggle('darkTheme');
+//   refs.modalCross.classList.toggle('darkTheme');
+//   refs.modalBG.classList.toggle('darkTheme');
+//   refs.buttonTheme.checked = false;
+// }
+
+// function themeChange() {
+//   if (refs.buttonTheme.checked) {
+//     darkTheme();
+//     setLocalStorage(Theme.DARK);
+//   } else {
+//     // darkTheme();
+
+//     lightTheme();
+//     setLocalStorage(Theme.LIGHT);
+//   }
+// }
+
+// function setLocalStorage(info) {
+//   localStorage.setItem('Theme:', info);
+// }
