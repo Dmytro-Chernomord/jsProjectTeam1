bd0ecca67ca39e29e7cb12cd5c6fca06629676b2
Dmytro <tschernomord@gmail.com>
2020-08-30 16:25:37 +0200

    static version

--- src/js/videoSlider.js
+++ src/js/videoSlider.js
@@ -1,10 +1,13 @@
+import videoSliderHandelbars from '../template/videoSwiper.hbs';
+
 const refs = {
   iframe: document.querySelector('iframe'),
-  swiperContainer: document.querySelector('.swiper-container'),
+  // swiperContainer: document.querySelector('.swiper-container'),
+  swiperWrapper: document.querySelector('.swiper-wrapper'),
 };
 let moviePlayUrl = '';
-refs.swiperContainer.addEventListener('mouseover', moviePlay);
-refs.swiperContainer.addEventListener('mouseout', stopMovie);
+refs.swiperWrapper.addEventListener('mouseover', moviePlay);
+refs.swiperWrapper.addEventListener('mouseout', stopMovie);
 function moviePlay() {
   const target = event.target;
   const url = target.getAttribute('src');
@@ -13,28 +16,27 @@ function moviePlay() {
 }
 
 function stopMovie() {
-  console.log(moviePlayUrl);
+  //   console.log(moviePlayUrl);
   const target = event.target;
   const stopPlay = target.setAttribute('src', moviePlayUrl);
 }
-// console.log(url);
-// const a = document.querySelector('iframe');
-// console.log(a);
-// refs.iframe.addEventListener('mouseover', test);
-// function test() {
-//   refs.iframe.setAttribute(
-//     'src',
-//     'http://www.youtube.com/embed/AZGcmvrTX9M?autoplay=1&enablejsapi=1&rel=0&mute=1&controls=0&fs=1&showinfo=0',
-//   );
-// }
 
-// refs.iframe.addEventListener('mouseout', test2);
-// function test2() {
-//   refs.iframe.setAttribute(
-//     'src',
-//     'http://www.youtube.com/embed/AZGcmvrTX9M?autoplay=0&enablejsapi=1&rel=0&mute=1',
-//   );
-// }
+let movieArray = [
+  'kGM4uYZzfu0',
+  'pAZGcmvrTX9M',
+  'xw1vQgVaYNQ',
+  'eLEwNo78f0k',
+  'n4YXauObskA',
+  'kGM4uYZzfu0',
+  't433PEQGErc',
+  'kzTktI1YNn4',
+  'u2ncERi6TgU',
+];
+const markupVideo = videoSliderHandelbars(movieArray);
+
+console.log(markupVideo);
+
+refs.swiperWrapper.insertAdjacentHTML('beforeend', markupVideo);
 
 var swiper = new Swiper('.swiper-container', {
   slidesPerView: 3,
@@ -47,16 +49,3 @@ var swiper = new Swiper('.swiper-container', {
   //   mousewheel: true,
   keyboard: true,
 });
-
-let movieArray = [
-  'kGM4uYZzfu0',
-  'pAZGcmvrTX9M',
-  'xw1vQgVaYNQ',
-  'eLEwNo78f0k',
-  'n4YXauObskA',
-  'kGM4uYZzfu0',
-  't433PEQGErc',
-  'kzTktI1YNn4',
-  'u2ncERi6TgU',
-  ' eyzxu26-Wqk',
-];
